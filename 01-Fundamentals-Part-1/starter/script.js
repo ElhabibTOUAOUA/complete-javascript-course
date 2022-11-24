96884a20cf7dcd76b3e032ab24144c2bd95b41d7
ElhabibTOUAOUA <bibahhabib8@gmail.com>
2022-11-24 08:08:14 +0100

    first-commit

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,62 @@
+/*
+const gravity = 9.80665;
+
+let data1 = {
+	mark: {
+		weigh: 78,
+		tall: 1.69,
+	},
+	john: {
+		weigh: 92,
+		tall: 1.95,
+	},
+};
+
+let data2 = {
+	mark: {
+		weigh: 95,
+		tall: 1.88,
+	},
+	john: {
+		weigh: 85,
+		tall: 1.76,
+	},
+};
+
+let mass = {
+	data1: {
+		mark: data1["mark"]["weigh"] / gravity,
+		john: data1["john"]["weigh"] / gravity,
+	},
+	data2: {
+		mark: data2["mark"]["weigh"] / gravity,
+		john: data2["john"]["weigh"] / gravity,
+	},
+};
+
+let BMI = {
+	data1: {
+		mark: mass["data1"]["mark"] / data1["mark"]["tall"] ** 2,
+		john: mass["data1"]["john"] / data1["john"]["tall"] ** 2,
+	},
+	data2: {
+		mark: mass["data2"]["mark"] / data2["mark"]["tall"] ** 2,
+		john: mass["data2"]["john"] / data2["john"]["tall"] ** 2,
+	},
+};
+
+let markHigherBMI = BMI["data1"]["mark"] > BMI["data1"]["john"];
+let markHigherBMI2 = BMI["data2"]["mark"] > BMI["data2"]["john"];
+
+console.log("Mark BMI: ", BMI["data1"]["mark"]);
+console.log("John BMI: ", BMI["data1"]["john"]);
+
+console.log(typeof BMI["data1"]["mark"]);
+console.log(markHigherBMI);
+console.log(markHigherBMI2);
+
+console.log(`Multi line
+string 
+is here`);
+
+*/

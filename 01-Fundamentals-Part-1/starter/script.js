7ff0c33b77ab7db430b0637348b6276d149fab63
ElhabibTOUAOUA <bibahhabib8@gmail.com>
2022-11-24 14:31:02 +0100

    final-changes

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,9 +54,96 @@ console.log("John BMI: ", BMI["data1"]["john"]);
 console.log(typeof BMI["data1"]["mark"]);
 console.log(markHigherBMI);
 console.log(markHigherBMI2);
-
-*/
-
 console.log(`Multi line
 string 
 is here`);
+
+function Avr(arr) {
+	return arr.reduce((partialSum, a) => partialSum + a, 0) / arr.length;
+}
+
+let data1 = {
+	dolphins: [96, 108, 89],
+	koalas: [88, 91, 110],
+};
+
+let data2 = {
+	dolphins: [97, 112, 101],
+	koalas: [109, 95, 123],
+};
+
+let data3 = {
+	dolphins: [97, 112, 101],
+	koalas: [109, 95, 106],
+};
+
+const dolphinsAvr = Avr(data2["dolphins"]);
+const koalasAvr = Avr(data2["koalas"]);
+
+
+// normal test of win
+if (dolphinsAvr === koalasAvr) console.log("There is a Draw!!!");
+else if (dolphinsAvr > koalasAvr) console.log("Dolphins Wins!!!");
+else console.log("Koalas Wins!!!");
+
+
+// bonus1 test of win
+if (dolphinsAvr === koalasAvr) console.log("There is a Draw!!!");
+else if (dolphinsAvr > koalasAvr && dolphinsAvr >= 100)
+	console.log("Dolphins Wins!!!");
+else if (koalasAvr > dolphinsAvr && koalasAvr >= 100)
+	console.log("Koalas Wins!!!");
+else console.log("There is no winner !!!");
+
+
+// bonus2 test of win
+if (dolphinsAvr === koalasAvr && dolphinsAvr >= 100) {
+	console.log(`Dolphins: ${dolphinsAvr} | Koalas: ${koalasAvr}`);
+	console.log("There is a Draw!!!");
+} else if (dolphinsAvr > koalasAvr && dolphinsAvr >= 100) {
+	console.log(`Dolphins: ${dolphinsAvr} | Koalas: ${koalasAvr}`);
+	console.log("Dolphins Wins!!!");
+} else if (koalasAvr > dolphinsAvr && koalasAvr >= 100) {
+	console.log(`Dolphins: ${dolphinsAvr} | Koalas: ${koalasAvr}`);
+	console.log("Koalas Wins!!!");
+} else console.log("There is no winner !!!");
+
+
+const days = [
+	"Saturday",
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+];
+
+let day = days[6];
+
+switch (day) {
+	case "Monday":
+		console.log("Plan course structure");
+		console.log("Go to coding meetup");
+		break;
+	case "Friday":
+		console.log("A rest day!!!🎞");
+		break;
+	default:
+		console.log("Not a valid day");
+}
+*/
+
+const bill = 430;
+const tip =
+	bill >= 50 && bill <= 300
+		? bill * 0.15
+		: bill < 50
+		? bill * 0.1
+		: bill * 0.2;
+
+console.log(
+	`The bill was ${bill}, the tip was ${tip}, and the total value was ${
+		bill + tip
+	}`
+);
